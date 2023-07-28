80a976416c59dad7c33fe4fe7c7926759499b2e0
kefimaher <kefi.maher@esprit.tn>
2023-07-28 07:08:39 +0100

    28/07/2023 07:08

--- Gstock.py
+++ Gstock.py
@@ -18,12 +18,8 @@ bg = ImageTk.PhotoImage(img)
 r.geometry("1500x800")
 background_label = tk.Label(r, image=bg)
 background_label.place(x=0, y=0, relwidth=1, relheight=1)
-
-
 # canva
 canvas = tk.Canvas(r, width=1500, height=800, background='grey')
-# line
-canvas.create_line(400, 0, 400, 800)
 # text
 txt = canvas.create_text(200, 100, text="Cible", font="Arial 16 italic", fill="black")
 # page content
@@ -33,15 +29,14 @@ canvas.create_rectangle(400,800,1500,0,fill="white")
 # barre de navigation a haut a droite
 canvas.create_rectangle(400,70,1500,0,fill="#55259F")
 # barre de navigation a haut a droite
-canvas.create_rectangle(0,70,400,0,fill="#492089")
+canvas.create_rectangle(0,70,400,0,fill="#50337D")
+# line vertical
+canvas.create_line(400, 150, 1500, 150,fill="#492089" , width=2)
 canvas.pack()
-
-
 # button name + width
 button = tk.Button(r, text='Close', width=10, command=r.destroy)
 # les cordonne du button
 button.place(x=1380,y=10)
-
 # button name + width
 button = tk.Button(r, text='maher', width=10, command="")
 # les cordonne du button
