e416b91bb6b9f2a9d2010d041372931bdb841ede
kefimaher <kefi.maher@esprit.tn>
2023-07-28 09:10:03 +0100

    28/07/2023 09:10

--- Gstock.py
+++ Gstock.py
@@ -16,7 +16,7 @@ canvas.create_rectangle(400,800,1500,0,fill="white")
 # barre de navigation a haut a droite
 canvas.create_rectangle(400,70,1500,0,fill="#55259F")
 # barre de navigation a haut a droite
-canvas.create_rectangle(0,70,400,0,fill="#492089")
+canvas.create_rectangle(0,70,400,0,fill="#4  92089")
 canvas.create_line(400,145,1500,145, fill="#492089", width=2)
 canvas.pack()
 
@@ -24,9 +24,17 @@ canvas.pack()
 # button name + width
 button = tk.Button(r, text='Close', width=10, command=r.destroy)
 # les cordonne du button
-button.place(x=1380,y=10)
+button.place(x=1380, y=10)
 # button name + width
-button = tk.Button(r, text='maher', width=10, command="")
+button = tk.Button(r, text='test 1', width=10, background="#683FA9", fg="white", highlightbackground="red", highlightcolor="red")
 # les cordonne du button
-button.place(x=10,y=10)
+button.place(x=1380, y=113)
+# button name + width
+button = tk.Button(r, text='test', width=10, background="#683FA9", fg="white")
+# les cordonne du button
+button.place(x=420, y=113)
+# button name + width
+button = tk.Button(r, text='test', width=10, background="#683FA9", fg="white")
+# les cordonne du button
+button.place(x=550, y=113)
 r.mainloop()
