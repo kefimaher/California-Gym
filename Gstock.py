9af7d19d08cfe88d99baacf1fb76bfd98e28d9b5
kefimaher <kefi.maher@esprit.tn>
2023-08-07 16:00:38 +0100

    7/8/2023 16:00

--- Gstock.py
+++ Gstock.py
@@ -3,13 +3,15 @@ from PIL import ImageTk, Image
 r = tk.Tk()
 # Titre du page
 r.title('Stock Net')
-# canva
 
-def cacher_rectangle():
-    canvas.itemconfigure(a, state=tk.HIDDEN)
+def toggle_page_visibility():
+    if page.winfo_viewable():
+        page.pack_forget()
+        setting.config(text="Show Page")
+    else:
+        page.pack()
+        setting.config(text="Hide Page")
 
-def afficher_rectangle():
-    canvas.itemconfigure(a, state=tk.NORMAL)
 canvas = tk.Canvas(r, width=1500, height=800, background='grey')
 
 # line
@@ -28,9 +30,6 @@ maher = canvas.create_text(200, 40, text="Stock Net", fill="white", font='Helvet
 
 
 
-a = canvas.create_rectangle(400, 10, 900, 500, fill="black")
-
-b = canvas.create_rectangle(400, 400, 600, 500, fill="red")
 
 # x = canvas.create_rectangle(400, 800, 1500, 146, fill="white")
 # y = canvas.create_rectangle(400, 800, 1500, 146, fill="grey")
@@ -42,28 +41,31 @@ logout.place(x=1380, y=10)
 search = tk.Button(r, text='Search', width=10, background="#683FA9", fg="white")
 search.place(x=1380, y=113)
 # button name + width
-test = tk.Button(r, text='test', width=10, background="#683FA9", fg="white")
-test.place(x=420, y=113)
+test1 = tk.Button(r, text='test1', width=10, background="#683FA9", fg="white")
+test1.place(x=420, y=113)
 # button name + width
-test2 = tk.Button(r, text='test 2', width=10, background="#683FA9", fg="white")
+test2 = tk.Button(r, text='test2', width=10, background="#683FA9", fg="white")
 test2.place(x=550, y=113)
 
 # button navbar a gauche vertical
 #Item list : afficher un page contien la list des produit dans la table (id,image,nom,prix,quantite)
-list = tk.Button(r, text='Items List', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white", command=afficher_rectangle())
-list.place(x=1, y=180)
+items = tk.Button(r, text='Items List', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white")
+items.place(x=1, y=180)
 #Button New items : ajouter un produit dans la table (id,image,nom,prix,quantite)
-newitems = tk.Button(r, text='New Items', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white", command=cacher_rectangle)
-newitems.place(x=1, y=230)
+news = tk.Button(r, text='New Items', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white")
+news.place(x=1, y=230)
 #Tout les fisher
 document = tk.Button(r, text='Document', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white")
 document.place(x=1, y=280)
 #Tout les entrant et sorant
 exp = tk.Button(r, text='Expences', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white")
-exp.place(x=1, y=330)
+exp .place(x=1, y=330)
 #Modifer
-setting = tk.Button(r, text='Setting', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white")
+setting = tk.Button(r, text='Setting', width=46, background="#3E4146", fg="white", highlightbackground="#3E4146", activebackground="#683FA9", activeforeground="white", command=toggle_page_visibility)
 setting.place(x=1, y=380)
+
+page = tk.Frame(r, width=1000, height=1000, bg="black")
+
 r.mainloop()
 # fg = coleur du text dans buton
 # background = coleur du button
