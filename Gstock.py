7c70118575d5b301be9f00444843ac037b236f5f
kefimaher <kefi.maher@esprit.tn>
2023-08-30 09:12:43 +0100

    09:12 30/08/2023

--- Gstock.py
+++ Gstock.py
@@ -217,12 +217,15 @@ r2 = tk.Label(dashbroad, text="Nombre des Pièce Trouve :", font=("Arial", 18), 
 r3 = tk.Label(dashbroad, text="Prix de Chaque Pièce :", font=("Arial", 18), fg="#683FA9", background="#BBBBBB")
 search_zone.bind("<FocusIn>", on_entry_click)
 #rectangele du tableu du bord
-dashbroadstatic1 = tk.Button(dashbroad, text='1', height=8, width=30,  background="#76448A", state='disabled')
-dashbroadstatic2 = tk.Button(dashbroad, text='2', height=8, width=30, background="#ADC9CB", state='disabled')
-dashbroadstatic3 = tk.Button(dashbroad, text='3', height=8, width=30, background="#A5CACC", state='disabled')
+
+dashbroadstatic1 = tk.Button(dashbroad, text='1', height=8, width=30,  background="#683FA9", state='disabled')
+dashbroadstatic2 = tk.Button(dashbroad, text='2', height=8, width=30, background="#683FA9", state='disabled')
+dashbroadstatic3 = tk.Button(dashbroad, text='3', height=8, width=30, background="#683FA9", state='disabled')
 produitarecherche  = tk.Entry(dashbroad, background="#BBBBBB", bd="0", fg="#683FA9", font=("Arial", 16), highlightbackground="#BBBBBB")
 dashbroadstatic1.place(x=100, y=100)
 dashbroadstatic2.place(x=400, y=100)
 dashbroadstatic3.place(x=700, y=100)
 r.mainloop()
+
+
 
